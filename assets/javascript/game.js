2645db7890ae672dc0dee371cad1106d0c0836a3
Sellieee <sellieee@hotmail.com>
2019-07-04 14:33:15 +1000

    created basic html file and create list of words for game

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+1. Select 40 foods for the word guess game
+
+var words = ["Burger", "Pizza", "Fries", "Fried Chicken", "Waffle", "Onion Rings", "Pancake", "Chicken Nugget", "Fish and Chips", "Icecream",
+"Soup", "Burrito", "Taco", "Sandwich", "Muffin", "Milkshake", "Kebab", "Dumpling", "Hot Dog", "Sausauge Roll", 
+"Pie", "Baguette", "Cookie", "Donut", "Bubble Tea", "Coffee", "Nacho", "Sushi", "Sashimi", "Halal Snack Pack",
+"Churro", "Wrap", "Roast Chicken", "Salad", "Spring Roll", "Banh Mi", "Gelato", "Loaded Fries", "Soft Frink", "Thickshake"]
